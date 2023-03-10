4d0a2f15b846f01ec82e5b75a06b65afc2e4dc12
James Xiong <jxiong783@gmail.com>
2023-03-09 22:16:10 -0800

    added node.js and routing to html pages

--- routes/htmls.js
+++ routes/htmls.js
@@ -1,2 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
+
+// routes /notes to the notes.html page
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+// routes to the index page
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const api = require('./routes/api.js');
+const htmls = require('./routes/htmls.js');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', api);
+app.use('/', htmls);
 
 // start server
 app.listen(PORT, () => console.log(`Opened at: ${PORT}`));
