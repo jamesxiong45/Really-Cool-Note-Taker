d875262424163ee0d5222821d04fc82d314bd9d9
James Xiong <jxiong783@gmail.com>
2023-03-09 22:45:44 -0800

    fixed css routing and added node.js files

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,67 @@
 const router = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+// route for getting all notes
+router.get('/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error getting notes' });
+      return;
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+// route for creating a new note
+router.post('/notes', (req, res) => {
+  const { title, text } = req.body;
+  if (!title || !text) {
+    res.status(400).json({ error: 'Please provide a title and text for the note' });
+    return;
+  }
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error getting notes' });
+      return;
+    }
+    const notes = JSON.parse(data);
+    const newNote = { title, text, id: uuidv4() };
+    notes.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error saving note' });
+        return;
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+// route for deleting a note
+router.delete('/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error getting notes' });
+      return;
+    }
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== noteId);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error deleting note' });
+        return;
+      }
+      res.sendStatus(204);
+    });
+  });
+});
+
+module.exports = router;
